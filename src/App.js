99ac9e1d662b227904fbf2e38e335cffa34c1327
amefuru <nanashipix@gmail.com>
2022-08-08 23:40:24 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+import React from 'react'
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const App = () => {
+    const onClickButton = () => {
+        alert();
+    };
+    return (
+    <>
+      <h1 style={{ color: 'red' }}>こんにちは</h1>
+      <p>テスト</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  )
+};
+
+export default App;
